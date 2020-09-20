363dd6b016baccb1297871c3fca05411dae1cf18
abrie <abrie@users.noreply.github.com>
2020-09-20 12:30:40 -0400

    Unit test for location.buildHttpRequest.

---
+++ src/github.com/abrie/mapatlapi/internal/location/buildHttpRequest_test.go
@@ -0,0 +1,34 @@
+package location
+
+import (
+	"bytes"
+	"context"
+	"testing"
+)
+
+func TestBuildHttpRequest(t *testing.T) {
+	request := Request{Ref_ID: 1001}
+	service := &Service{Endpoint: "https://local.dev"}
+
+	httpRequest, err := request.BuildHttpRequest(context.Background(), service)
+	if err != nil {
+		t.Errorf("Unexpected error: %w", err)
+	}
+
+	expectedUrl := service.Endpoint
+	gotUrl := httpRequest.URL.String()
+
+	if gotUrl != expectedUrl {
+		t.Errorf("\nWant:\t'%s'\nGot:\t'%s'", expectedUrl, gotUrl)
+	}
+
+	expectedBody := "wsparam%5B%5D=1001"
+	gotBody := new(bytes.Buffer)
+	if _, err := gotBody.ReadFrom(httpRequest.Body); err != nil {
+		t.Errorf("Unexpected error: %w", err)
+	}
+
+	if gotBody.String() != expectedBody {
+		t.Errorf("\nWant:\t%s'\nGot:\t'%s'", expectedBody, gotBody.String())
+	}
+}
